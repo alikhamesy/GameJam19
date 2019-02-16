046854ab486662816ec89816053cf193073d21de
JoBapla <jovanbapla@gmail.com>
2019-02-16 00:03:28 -0800

    Added scripts folder

---
+++ gamejam19/Assets/Scripts/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    float speed = 5.0f;
+    // Start is called before the first frame update
+    public Animator animator;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        Vector3 Horizontal = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position += Horizontal *speed* Time.deltaTime;
+        
+    }
+}
