10a033f9043a7f9642bb4c69e61d652a6a012f32
alikhamesy <akhamesy@sfu.ca>
2019-02-16 12:33:28 -0800

    basic pick up script

---
+++ gamejam19/Assets/Scripts/pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((Input.GetKeyDown(KeyCode.E)))
+            this.gameObject.transform.parent = collision.gameObject.transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
